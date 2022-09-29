d45ba86fc4e9f1c2ce85b51e81bb9cf6f1a24440
Brian Elizondo <brian_elizondo@msn.com>
2022-09-29 17:01:21 -0400

    Data Structures => Sorting Algorithms Exercises: insertionSort, Here's some guidance for how insertion sort should work: Start by picking the second element in the array (we will assume the first element is the start of the 'sorted' portion). Now compare the second element with the one before it and swap if necessary. Continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place. Repeat until the array is sorted

--- insertion.js
+++ insertion.js
@@ -1,3 +1,22 @@
-function insertionSort() {}
+// insertionSort
+// Here’s some guidance for how insertion sort should work:
+// Start by picking the second element in the array (we will assume the first element is the start of the “sorted” portion)
+// Now compare the second element with the one before it and swap if necessary.
+// Continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place.
+// Repeat until the array is sorted.
+function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        for(let j = i; j >= 0; j--){
+            if(arr[j - 1] > arr[j]){
+                let temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }else{
+                break;
+            }
+        }
+    }
+    return arr;
+}
 
 module.exports = insertionSort;
