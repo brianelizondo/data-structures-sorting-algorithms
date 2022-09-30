ec66f19fa7b7bf1802c03fe1496b99bf8e034614
Brian Elizondo <brian_elizondo@msn.com>
2022-09-30 19:52:02 -0400

    Data Structures => Sorting Algorithms Exercises: pivot, In this exercise, your goal is to implement a function called pivot. This function contains nearly all of the logic you'll need in order to implement Quick Sort in the next exercise. The pivot function is responsible for taking an array, setting the pivot value, and mutating the array so that all values less than the pivot wind up to the left of it, and all values greater than the pivot wind up to the right of it. It’s also helpful if this helper returns the index of where the pivot value winds up

--- quick.js
+++ quick.js
@@ -2,8 +2,21 @@
 pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
-
-function pivot(){}
+function pivot(arr){
+    let pivotValue = arr[0];
+    let pivotIndex = 0; 
+    
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] < pivotValue) {
+            pivotIndex++;
+            [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+        }
+    }
+    
+    [arr[0], arr[pivotIndex]] = [arr[pivotIndex], arr[0]];
+    
+    return pivotIndex;
+}
 
 /*
 quickSort accepts an array, left index, and right index
@@ -11,4 +24,4 @@ quickSort accepts an array, left index, and right index
 
 function quickSort() {}
 
-module.exports = quickSort;
+module.exports = {pivot, quickSort};
